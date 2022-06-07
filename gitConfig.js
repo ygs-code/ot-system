31543175b46b495a96fa1991405a3859ddc3a4e1
姚观寿 <yaoguanshou@rainbowcn.com>
2022-06-07 10:11:25 +0800

    自动化脚本

---
+++ gitConfig.js
@@ -0,0 +1,25 @@
+/*
+ * @Date: 2022-06-06 10:28:48
+ * @Author: Yao guan shou
+ * @LastEditors: Yao guan shou
+ * @LastEditTime: 2022-06-06 16:09:29
+ * @FilePath: /Blogs/gitConfig.js
+ * @Description:
+ */
+module.exports = [
+  {
+    name: '后台管理系统',
+    dir: 'BlogsAdmin',
+    git: 'git@github.com:qq281113270/BlogsAdmin.git',
+  },
+  {
+    name: '客户端',
+    dir: 'BlogsClient',
+    git: 'git@github.com:qq281113270/BlogsClient.git',
+  },
+  {
+    name: '后台服务',
+    dir: 'BlogsServer',
+    git: 'git@github.com:qq281113270/BlogsServer.git',
+  },
+]
