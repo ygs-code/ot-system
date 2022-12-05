2b7ca570e18f9b1f738c72b1234521093c4a6db7
姚观寿 <yaoguanshou@rainbowcn.com>
2022-12-05 18:01:52 +0800

    添加stylelint 校验

--- BlogsAdmin1/webpack/config/client/webpack.base.config.js
+++ BlogsAdmin1/webpack/config/client/webpack.base.config.js
@@ -357,16 +357,17 @@ module.exports = {
 
     new webpack.HashedModuleIdsPlugin(), // 确保 hash 不被意外改变
     // stylelint 插件
-    // new StylelintPlugin({
-    //   emitError: true, //发现的错误将始终被触发，将禁用设置为false。
-    //   emitWarning: true, //如果将disable设置为false，则发现的警告将始终被发出。
-    //   failOnError: true, //如果有任何错误，将导致模块构建失败，禁用设置为false。
-    //   failOnWarning: false, //如果有任何警告，如果设置为true，将导致模块构建失败。
-    //   quiet: false, //如果设置为true，将只处理和报告错误，而忽略警告。
-    //   fix: true //自动修复
-    //   // stylelintPath:'',
-    //   // extensions: ["dist/server/static/css/*.css"] // 排除目录检查
-    // }),
+    new StylelintPlugin({
+      emitError: true, //发现的错误将始终被触发，将禁用设置为false。
+      emitWarning: true, //如果将disable设置为false，则发现的警告将始终被发出。
+      failOnError: true, //如果有任何错误，将导致模块构建失败，禁用设置为false。
+      failOnWarning: false, //如果有任何警告，如果设置为true，将导致模块构建失败。
+      quiet: false, //如果设置为true，将只处理和报告错误，而忽略警告。
+      fix: true, //自动修复
+      files: path.resolve(rootPath, "client/**/*.(less|css)")
+      // stylelintPath:'',
+      // extensions: ["dist/server/static/css/*.css"] // 排除目录检查
+    }),
     // eslint 插件
     new ESLintPlugin({
       emitError: true, //发现的错误将始终被触发，将禁用设置为false。
--- BlogsAdmin1/webpack/config/client/webpack.dev.config.js
+++ BlogsAdmin1/webpack/config/client/webpack.dev.config.js
@@ -66,6 +66,7 @@ module.exports = {
             loader: "css-loader",
             options: {
               sourceMap: true
+
             }
           },
           {
