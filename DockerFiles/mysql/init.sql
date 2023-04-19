23eb241be3719665e834a21e011b6a7aaca30a3a
姚观寿 <yaoguanshou@rainbowcn.com>
2023-04-19 18:16:04 +0800

    docker 配置

---
+++ DockerFiles/mysql/init.sql
@@ -0,0 +1,32 @@
+create database if not exists admin;
+-- use admin;
+
+-- SET NAMES utf8mb4;
+-- SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
+-- DROP TABLE IF EXISTS `admin`;
+-- CREATE TABLE `admin`
+-- (
+--     `id`     INT AUTO_INCREMENT PRIMARY KEY,
+--     `name`   VARCHAR(10) NOT NULL COMMENT '元素名称',
+--     `status` VARCHAR(10) NOT NULL COMMENT '元素状态',
+--     `icon`   VARCHAR(50) NOT NULL COMMENT 'ICON',
+--     `type`   VARCHAR(50) NOT NULL COMMENT '类型',
+--     CONSTRAINT x_item_name_uindex UNIQUE (name)
+-- ) ENGINE = InnoDB
+--   CHARACTER SET = utf8mb4 COMMENT 'X子项'
+--   ROW_FORMAT = Dynamic;
+
+-- -- 初始化 X子项
+-- INSERT INTO `x_item`(`id`, `name`, `status`, `icon`, `type`) VALUES (27, 'XItem02', 'ENABLE', 'ITEM_9', 'ITEM_9');
+-- INSERT INTO `x_item`(`id`, `name`, `status`, `icon`, `type`) VALUES (28, 'XItem03', 'ENABLE', 'ITEM_10', 'ITEM_10');
+-- INSERT INTO `x_item`(`id`, `name`, `status`, `icon`, `type`) VALUES (29, 'XItem04', 'ENABLE', 'ITEM_11', 'ITEM_11');
+-- INSERT INTO `x_item`(`id`, `name`, `status`, `icon`, `type`) VALUES (30, 'XItem05', 'ENABLE', 'ITEM_12', 'ITEM_12');
+-- INSERT INTO `x_item`(`id`, `name`, `status`, `icon`, `type`) VALUES (31, 'XItem06', 'ENABLE', 'ITEM_13', 'ITEM_13');
+-- INSERT INTO `x_item`(`id`, `name`, `status`, `icon`, `type`) VALUES (32, 'XItem07', 'ENABLE', 'ITEM_14', 'ITEM_14');
+-- INSERT INTO `x_item`(`id`, `name`, `status`, `icon`, `type`) VALUES (33, 'XItem08', 'ENABLE', 'ITEM_15', 'ITEM_15');
+-- INSERT INTO `x_item`(`id`, `name`, `status`, `icon`, `type`) VALUES (34, 'XItem08', 'ENABLE', 'ITEM_16', 'ITEM_16');
+
