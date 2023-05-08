e6ccc263350930a4181bc5b1df6dd3fd910fc6dd
yao guan shou <281113270@qq.com>
2023-05-08 23:30:40 +0800

    ci: 更改shell脚本

--- bin/gitConfig.js
+++ bin/gitConfig.js
@@ -9,25 +9,24 @@
  * 如果更新有问题 请把git地址前缀 git@github.com:ygs-code/ 替换成 https://github.com/qq281113270/
   用http下载
  */
-
 module.exports = [
     {
         name: '后台管理系统',
         container_name: 'admin',
         dir: 'ot-system-admin',
-        git: 'https://github.com/ygs-code/ot-system-admin.git',
+        git: 'git@github.com:ygs-code/ot-system-admin.git',
     },
     {
         name: '后台服务',
         container_name: 'server',
         dir: 'ot-system-server',
-        git: 'https://github.com/ygs-code/ot-system-server.git',
+        git: 'git@github.com:ygs-code/ot-system-server.git',
         isDockerBuild: true,
     },
     {
         name: '客户端',
         container_name: 'client',
         dir: 'ot-system-client',
-        git: 'https://github.com/ygs-code/ot-system-client.git',
+        git: 'git@github.com:ygs-code/ot-system-client.git',
     },
 ];
--- bin/updateCode.js
+++ bin/updateCode.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-const gitConfig = require("./gitConfig");
-const { execute } = require("./cmd"); //
-const path = require("path");
-const fs = require("fs");
+const gitConfig = require('./gitConfig');
+const { execute } = require('./cmd'); //
+const path = require('path');
+const fs = require('fs');
 const { readdirSync, stat } = fs;
-const chalk = require("chalk");
+const chalk = require('chalk');
 
 const {
-  env: { type },
+    env: { type },
 } = process;
 
 /*
@@ -16,127 +16,135 @@ __filename返回放置文件的绝对路径。
 __dirname返回__filename目录的绝对路径。
 */
 class UpdateCode {
-  constructor() {
-    this.addReg = /git add/gi;
-    this.pushReg = /git push/gi;
-    this.committedReg = /committed/gi;
-    this.spinner;
-    // this.init();
-  }
-  async gitClonePull(type) {
-    for (let item of gitConfig) {
-      let { name, dir, git, container_name } = item;
-      // 如果有配置单独发布则不会更新其他模块
-      if (
-        type &&
-        (type !== "publish_all" || type !== "bulid_code") &&
-        type != container_name
-      ) {
-        continue;
-      }
-      // console.log('item==',item)
-      process.chdir(path.join(__dirname, "../"));
-      dir = path.join(__dirname, "../", dir);
-      await new Promise((resolve, reject) => {
-        // console.log('dir=', path.join(__dirname,'../',dir));
-        stat(dir, async (err, stats) => {
-          if (!err && stats.isDirectory()) {
-            // 更新
-            await this.gitPull({
-              name,
-              dir,
-              git,
-            });
-            resolve();
-          } else {
-            // 克隆
-            await this.gitClone({
-              name,
-              dir,
-              git,
-            });
-            resolve();
-          }
-        });
-      });
+    constructor() {
+        this.addReg = /git add/gi;
+        this.pushReg = /git push/gi;
+        this.committedReg = /committed/gi;
+        this.spinner;
+        // this.init();
     }
-  }
+    async gitClonePull(type) {
+        for (let item of gitConfig) {
+            let { name, dir, git, container_name } = item;
+            // 如果有配置单独发布则不会更新其他模块
+            if (
+                type &&
+                !(
+                    type == 'update_code' ||
+                    type == 'publish_all' ||
+                    type == 'bulid_code'
+                ) &&
+                type != container_name
+            ) {
+                continue;
+            }
 
-  async gitClone({ name, dir, git }) {
-    console.log(
-      chalk.rgb(17, 168, 203)(`\n开始克隆:${name}\n  git源地址：${git}\n`)
-    );
-    // this.spinner = ora(chalk.rgb(17, 168, 203)('克隆代码中请稍后.....\n'));
-    // this.spinner.start();
-    // process.chdir('../');
-    await this.PromiseExec(`git clone ${git}`);
+            console.log('item==', item);
+            process.chdir(path.join(__dirname, '../'));
+            dir = path.join(__dirname, '../', dir);
+            await new Promise((resolve, reject) => {
+                // console.log('dir=', path.join(__dirname,'../',dir));
+                stat(dir, async (err, stats) => {
+                    if (!err && stats.isDirectory()) {
+                        // 更新
+                        await this.gitPull({
+                            name,
+                            dir,
+                            git,
+                        });
+                        resolve();
+                    } else {
+                        // 克隆
+                        await this.gitClone({
+                            name,
+                            dir,
+                            git,
+                        });
+                        resolve();
+                    }
+                });
+            });
+        }
+    }
 
-    // console.log('克隆成功')
-    process.chdir(dir);
-    let { stdout: remote } = await this.PromiseExec(`git remote -v`, {
-      stdio: null,
-    });
-    remote = remote.split("\n")[1];
-    let { stdout: branch } = await this.PromiseExec(`git branch`, {
-      stdio: null,
-    });
-    branch = branch.toString().match(/(?<=\*)\s*\w+/)[0];
-    console.log(
-      chalk.rgb(
-        13,
-        188,
-        121
-      )(`克隆:${name}成功\n  git源地址：${remote}\n  git分支:${branch}`)
-    );
-    // this.spinner.stop();
-  }
-  async gitPull({ name, dir, git }) {
-    process.chdir(dir);
-    // let { stdout: remote } =
-    // await this.PromiseExec(`git remote -v`, {
-    // //   stdio: null,
-    // });
-    // remote = remote.split("\n")[1];
+    async gitClone({ name, dir, git }) {
+        console.log(
+            chalk.rgb(17, 168, 203)(`\n开始克隆:${name}\n  git源地址：${git}\n`)
+        );
+        // this.spinner = ora(chalk.rgb(17, 168, 203)('克隆代码中请稍后.....\n'));
+        // this.spinner.start();
+        // process.chdir('../');
+        await this.PromiseExec(`git clone ${git}`);
 
-    // let { stdout: branch } =
-    console.log(`更新:${name}\n git源地址：${git}\n `);
-    console.log("git 分支:");
-    await this.PromiseExec(`git branch`, {
-      //   stdio: null,
-    });
+        // console.log('克隆成功')
+        process.chdir(dir);
+        let { stdout: remote } = await this.PromiseExec(`git remote -v`, {
+            stdio: null,
+        });
+        remote = remote.split('\n')[1];
+        let { stdout: branch } = await this.PromiseExec(`git branch`, {
+            stdio: null,
+        });
+        branch = branch.toString().match(/(?<=\*)\s*\w+/)[0];
+        console.log(
+            chalk.rgb(
+                13,
+                188,
+                121
+            )(`克隆:${name}成功\n  git源地址：${remote}\n  git分支:${branch}`)
+        );
+        // this.spinner.stop();
+    }
+    async gitPull({ name, dir, git }) {
+        process.chdir(dir);
+        // let { stdout: remote } =
+        // await this.PromiseExec(`git remote -v`, {
+        // //   stdio: null,
+        // });
+        // remote = remote.split("\n")[1];
 
-    console.log(`更新代码中\n `);
-    await this.PromiseExec(`git pull`);
-    console.log(`${name} 代码更新成功\n\n`);
-  }
-  async init($type) {
-    await this.gitClonePull($type || type);
-  }
+        // let { stdout: branch } =
+        console.log(`更新:${name}\n git源地址：${git}\n `);
+        console.log('git 分支:');
+        await this.PromiseExec(`git branch`, {
+            //   stdio: null,
+        });
 
-  // 执行程序
-  PromiseExec(cmd, options = {}) {
-    const { getStdout = () => {}, callback = () => {} } = options;
-    return new Promise((reslove, reject) => {
-      execute(cmd, {
-        // stdio: null,
-        ...options,
-        getStdout: (stdout) => {
-          getStdout(stdout);
-          callback();
-          reslove({
-            cmd,
-            stdout,
-            // stderr,
-            code: 200,
-          });
-        },
-        callback: () => {
-          callback();
-          reslove();
-        },
-      });
-    });
-  }
+        console.log(`更新代码中\n `);
+        console.log(1);
+        await this.PromiseExec(`git pull`);
+        console.log(`${name} 代码更新成功\n\n`);
+    }
+    async init($type) {
+ 
+        await this.gitClonePull($type || type);
+    }
+
+    // 执行程序
+    PromiseExec(cmd, options = {}) {
+        console.log(1);
+        const { getStdout = () => {}, callback = () => {} } = options;
+        return new Promise((reslove, reject) => {
+            execute(cmd, {
+                // stdio: null,
+                ...options,
+                getStdout: (stdout) => {
+                    getStdout(stdout);
+                    callback();
+                    reslove({
+                        cmd,
+                        stdout,
+                        // stderr,
+                        code: 200,
+                    });
+                },
+                callback: () => {
+                    callback();
+                    reslove();
+                },
+            });
+        });
+    }
 }
 
 module.exports = new UpdateCode();
